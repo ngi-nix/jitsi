801ead69d7c7c4689a6b768ae95ce9432ecdce57
tshaynik <tshaynik@protonmail.com>
2021-09-09 13:20:11 -0400

    remove extra config
    This flake should only output a module

--- flake.nix
+++ flake.nix
@@ -2,26 +2,6 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
 
   outputs = { self, nixpkgs }: {
-
     nixosModules.jitsi = import nix/module.nix;
-
-    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules =
-        [ ({ pkgs, ... }: {
-            boot.isContainer = true;
-
-            # Let 'nixos-version --json' know about the Git revision
-            # of this flake.
-            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-
-            # Network configuration.
-            networking.useDHCP = false;
-            networking.firewall.allowedTCPPorts = [ 80 ];
-
-           })
-        ];
-    };
-
   };
 }
--- nix/module.nix
+++ nix/module.nix
@@ -4,12 +4,9 @@
   config = {
     services.jitsi-meet = {
       enable = true;
-      hostName = "jitsi.example.com";
     };
 
     services.jitsi-videobridge.openFirewall = true;
     networking.firewall.allowedTCPPorts = [ 80 443 ];
-    security.acme.email = "me@example.com";
-    security.acme.acceptTerms = true;
   };
 }
