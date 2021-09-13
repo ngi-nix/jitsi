9a0efdd799e81b293ff3382d047db8937f327056
tshaynik <tshaynik@protonmail.com>
2021-09-13 13:54:25 -0400

    use tshaynik's fork of nixpkgs for testing

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+  inputs.nixpkgs.url = "github:tshaynik/nixpkgs/jitsi-6293";
 
   outputs = { self, nixpkgs }: {
     nixosModules.jitsi = import nix/module.nix;
