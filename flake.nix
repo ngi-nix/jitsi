af483ae7c8e5700a81451abcc9696c92d6d7bcad
tshaynik <tshaynik@protonmail.com>
2021-09-27 13:10:18 -0400

    create overlay to use jitsi-meet branch with breakout rooms

--- flake.nix
+++ flake.nix
@@ -2,6 +2,24 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
 
   outputs = { self, nixpkgs }: {
+    overlay = self: super: {
+      jitsi-meet = super.jitsi.override  (old: {
+        src = super.fetchFromGitHub {
+          owner = "jitsi";
+          repo = "jitsi-meet";
+          rev = "e03d32cdcf62479ba2ec29418df046d652aa9266";
+          # If you don't know the hash, the first time, set:
+          # sha256 = "0000000000000000000000000000000000000000000000000000";
+          # then nix will fail the build with such an error message:
+          # hash mismatch in fixed-output derivation '/nix/store/m1ga09c0z1a6n7rj8ky3s31dpgalsn0n-source':
+          # wanted: sha256:0000000000000000000000000000000000000000000000000000
+          # got:    sha256:173gxk0ymiw94glyjzjizp8bv8g72gwkjhacigd1an09jshdrjb4
+          sha256 = "0000000000000000000000000000000000000000000000000000";
+        };
+      });
+
+
+    }
     nixosModules.jitsi = import nix/module.nix;
     nixosModule = self.nixosModules.jitsi;
   };
